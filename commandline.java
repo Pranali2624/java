b52902bdf143d2f518145258df71d5d395c189cb
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2025-03-31 22:31:46 +0530

    Add files via upload

---
+++ commandline.java
@@ -0,0 +1,7 @@
+public class commandline {
+    public static void main(String[] args) {
+        System.out.println("Command line Argument Are ");
+        for(String str: args)
+        System.out.println(str);
+    }
+}
